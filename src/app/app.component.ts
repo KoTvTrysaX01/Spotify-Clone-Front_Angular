a85b83f7c20f62eb8278690bdc835bdd65ade9a3
Elshin Vadim <elshinvadim@gmail.com>
2025-09-09 19:14:27 +0200

    Creating the frontend project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {fontAwesomeIcons} from "./shared/font-awesome-icons";
+import {NavigationComponent} from "./layout/navigation/navigation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [FontAwesomeModule, RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'spotify-clone-front';
+  private faIconLibrary = inject(FaIconLibrary)
+
+  ngOnInit(): void {
+    this.initFromAwesome();
+  }
+
+  private initFromAwesome(){
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,7 @@
+import {IconDefinition, faHome, faSearch, faCirclePlay} from "@fortawesome/free-solid-svg-icons";
+
+export const fontAwesomeIcons: IconDefinition[] = [
+  faCirclePlay,
+  faHome,
+  faSearch
+]
